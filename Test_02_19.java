20393c4a73b003f6f70223557385c82468aca0bb
tianfahreza-pro <tianfahreza240@gmail.com>
2025-10-02 15:08:25 +0700

    Tian_19 Membuat perubahan

--- Test_02_19.java
+++ Test_02_19.java
@@ -2,5 +2,6 @@ public class Test_02_19 {
 
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama");
+        System.out.println("ini baris kedua");
     }
 }

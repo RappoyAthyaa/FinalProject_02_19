6d0098406e6aeab3f05921407c984c7b99c38640
RappoyAthyaa <raffbrtd.18@gmail.com>
2025-10-02 15:34:46 +0700

    Athaya_02 membuat perubahan

--- Test_02_19.java
+++ Test_02_19.java
@@ -1,7 +1,7 @@
 public class Test_02_19 {
 
     public static void main(String[] args) {
-        System.out.println("Hi, ini baris kode pertama");
-        System.out.println("ini baris kedua");
+        System.out.println("Hi, ini baris kode pertama yang dimodifikasi User Athaya_02");
+        System.out.println("User Athaya_02 menambahkan baris baru");
     }
 }

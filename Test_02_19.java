b181eb7c99d4ddfe818b7d179747466a940594b5
tianfahreza-pro <tianfahreza240@gmail.com>
2025-10-02 15:34:48 +0700

    Tian_19 membuat perubahan

--- Test_02_19.java
+++ Test_02_19.java
@@ -1,7 +1,7 @@
 public class Test_02_19 {
 
     public static void main(String[] args) {
-        System.out.println("Hi, ini baris kode pertama");
-        System.out.println("ini baris kedua");
+        System.out.println("Hi, ini baris kode pertama yang dimodifikasi User Tian_19");
+        System.out.println("User Tian_19 menambahkan baris baru");
     }
 }
